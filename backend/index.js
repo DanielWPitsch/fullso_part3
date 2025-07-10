15dd72dcaf1b3c71413b46af877c9cd8ac5ceed0
Daniel Warella Pitsch <daniel.pitsch@SERPB.local>
2025-07-10 14:52:19 -0300

    front e back

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 
 const app = express()
+app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('body', (req) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : ''
